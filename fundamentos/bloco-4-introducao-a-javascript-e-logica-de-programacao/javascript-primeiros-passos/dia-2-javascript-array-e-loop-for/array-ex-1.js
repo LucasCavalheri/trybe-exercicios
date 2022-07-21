ecc1332e0714c4462260d83a91645f7951842fd2
LucasCavalheri <cav.catecumenal@gmail.com>
2022-07-21 15:14:47 -0300

    Finalizando o Exercício 2.

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/javascript-primeiros-passos/dia-2-javascript-array-e-loop-for/array-ex-1.js
+++
@@ -1,6 +0,0 @@
-// Obtenha o valor "Serviços" do array menu:
-
-let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
-let menuServices = menu.indexOf('Serviços');
-
-console.log(menuServices);
---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/javascript-primeiros-passos/dia-2-javascript-array-e-loop-for/arrays-exercicios.js
@@ -0,0 +1,13 @@
+// Obtenha o valor "Serviços" do array menu:
+
+var menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let menuServices = menu.indexOf('Serviços');
+
+console.log(menuServices);
+
+// Procure o índice do valor "Portfólio" do array menu:
+
+var menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let indexOfPortfolio = menu.indexOf('Portfólio');
+
+console.log(indexOfPortfolio);
