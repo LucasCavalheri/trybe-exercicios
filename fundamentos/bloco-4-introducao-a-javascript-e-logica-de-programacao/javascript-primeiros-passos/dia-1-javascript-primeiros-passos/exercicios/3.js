6a6a9d68379c65fb305ffad6e674c32143a4e48e
LucasCavalheri <cav.catecumenal@gmail.com>
2022-07-21 11:32:04 -0300

    Finalizando o Exercicio 3.

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/javascript-primeiros-passos/dia-1-javascript-primeiros-passos/exercicios/3.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/javascript-primeiros-passos/dia-1-javascript-primeiros-passos/exercicios/3.js
@@ -3,3 +3,12 @@
 const valor1 = 10;
 const valor2 = 20;
 const valor3 = 30;
+
+
+if (valor1 > valor2 && valor1 > valor3){
+    console.log(valor1);
+} else if (valor2 > valor1 && valor2 > valor3){
+    console.log(valor2)
+} else {
+    console.log(valor3);
+}
