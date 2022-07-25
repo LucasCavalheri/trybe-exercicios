adfa4869796d4e06c0a57c592a2a0a11dbcc67c5
LucasCavalheri <cav.catecumenal@gmail.com>
2022-07-25 18:21:59 -0300

    Finalizando os Exercícios 1, 2, 3 e 4

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javacript-objetos-e-funcoes/exercicios.js
@@ -0,0 +1,39 @@
+function separa(){
+    console.log('-------------------------------------------------');
+}
+
+// 1 - Imprima no console uma mensagem de boas-vindas para a personagem acima, incluindo seu nome. Valor esperado no console:
+
+let info = {
+    personagem: 'Margarida',
+    origem: 'Pato Donald',
+    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+  };
+
+console.log(`Bem-vinda, ${info.personagem}`);
+
+separa();
+
+// 2 - Insira no objeto uma nova propriedade com o nome de chave 'recorrente' e o valor 'Sim' e, em seguida, imprima o objeto no console. Valor esperado no console:
+
+info.recorrente = 'Sim';
+
+console.log(info.recorrente);
+
+separa();
+
+// 3 - Faça um for/in que mostre todas as chaves do objeto. Valor esperado no console:
+
+for (let key in info) {
+    console.log(key);
+}
+
+separa();
+
+// 4 - Faça um novo for/in, mas agora mostre todos os valores das chaves do objeto. Valor esperado no console:
+
+for (let key in info) {
+    console.log(info[key]);
+}
+
+separa();
