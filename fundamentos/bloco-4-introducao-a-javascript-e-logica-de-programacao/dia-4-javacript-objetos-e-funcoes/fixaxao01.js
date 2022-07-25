efa948d2c394d8ef1b111e98f30ace6d54ab85a8
LucasCavalheri <cav.catecumenal@gmail.com>
2022-07-25 13:44:06 -0300

    Finalizando Fixação 04

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javacript-objetos-e-funcoes/fixaxao01.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javacript-objetos-e-funcoes/fixaxao01.js
@@ -16,3 +16,7 @@ let player = {
 console.log(`A jogadora ${player.name} tem ${player.age} anos de idade.`);
 
 /* 3 - Adicione ao objeto a chave bestInTheWorld e atribua a esta chave um array contendo as datas em que a jogadora Marta foi considerada a melhor do mundo. */
+
+/* 4 - Acesse a chave bestInTheWorld e faça um console.log no seguinte formato: "A jogadora Marta Silva foi eleita a melhor do mundo por 6 vezes". */
+
+console.log(`A jogadora ${player['name']} foi eleita a melhor do mundo por ${player.bestInTheWorld.length} vezes`);
